72f4f4b4a2b442b1ed0c4bfe93100169d89b7194
rpastorvargas <rpastor@scc.uned.es>
2015-10-01 22:24:33 +0200

    Only assests in www folder

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\users\rafa\RLAB_Xml\HTML5\phonegap\rlabmobile\platforms\android\bin\AndroidManifest.xml : \
-D:\users\rafa\RLAB_Xml\HTML5\phonegap\rlabmobile\platforms\android\AndroidManifest.xml \
--- platforms/android/gen/es/uned/scc/related/mobile/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package es.uned.scc.related.mobile;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
